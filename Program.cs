33de92b19dacda76d410c0e77e2a030e8f2965cb
MurariuF <murariu.flavius@gmail.com>
2025-05-19 17:50:34 +0300

    Created menu and methods for menu's options

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Data.Sqlite;
+using SQLitePCL;
 
 namespace HabitTracker
 {
@@ -6,16 +7,65 @@ namespace HabitTracker
     {
         static void Main(string[] args)
         {
-            var connectionString = @"DataSource=habittracker.db";
+
+            Batteries.Init();
+            var connectionString = @"DataSource=habitTracker.db";
 
             using (var connection = new SqliteConnection(connectionString))
             {
                 connection.Open();
 
                 var tableCommand = connection.CreateCommand();
-                tableCommand.CommandText = "";
+                tableCommand.CommandText = @"CREATE TABLE IF NOT EXISTS drinking_water(
+                                                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                                Date TEXT,
+                                                Qunatity INTEGER
+                                             )";
 
+                tableCommand.ExecuteNonQuery();
+                
+                connection.Close();
+            }
+        }
 
+        static void GetUserInput()
+        {
+            Console.Clear();
+            bool closeApp = false;
+            while (closeApp == false)
+            {
+                Console.WriteLine("\n\nMain Menu");
+                Console.WriteLine("\nWhat would you like to do?");
+                Console.WriteLine("\nType 0 to Close Application");
+                Console.WriteLine("\n1. View all records");
+                Console.WriteLine("\n2. Insert record");
+                Console.WriteLine("\n3. Delete record");
+                Console.WriteLine("\n4. Update record");
+
+                string commandInput = Console.ReadLine();
+
+                switch (commandInput)
+                {
+                    case 0:
+                        Console.WriteLine("\n\nClosing Application");
+                        closeApp = true;
+                        break;
+                    case 1:
+                        GetAllRecords();
+                        break;
+                    case 2:
+                        InsertRecord();
+                        break;
+                    case 3:
+                        DeleteRecord();
+                        break;
+                    case 4:
+                        UpdateRecord();
+                        break;
+                    default:
+                        Console.WriteLine("\n\nInvalid command. Please type a number between 0 and 4");
+                        break;
+                }
             }
         }
     }
