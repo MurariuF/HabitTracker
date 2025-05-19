b079811918ff248c0a9d944e5033b3b9fbe8a417
MurariuF <murariu.flavius@gmail.com>
2025-05-19 15:46:51 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace HabitTracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = @"DataSource=habittracker.db";
+
+            using(var connection = new SqliteConnection(connectionString)) {
+                connection.Open();
+
+                var tableCommand = connection.CreateCommand();
+                tableCommand.CommandText = "";
+
+
+        }
+    }
+}
